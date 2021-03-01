85261ed151faac24c412abf7e74d8ebffd75ebd3
Dika Handika <dika.handika@paper.id>
2021-03-01 22:41:35 +0700

    AlbumService + AlbumComponent

--- src/pages/album/Albums.tsx
+++ src/pages/album/Albums.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AlbumService, { Album } from './../../services/AlbumService'
 
 export default function Albums() {
-  return (
-    <div>
-      <p>Albums works!</p>
-    </div>
-  )
+  const [albums, setAlbums] = useState<Album[]>([])
+  const albumService = new AlbumService()
+
+  useEffect(() => {
+    albumService.getAllAlbums().subscribe((response) => {
+      console.log(response)
+
+      setAlbums(response)
+    })
+  }, [])
+
+  const albumLists = albums.map((album) => (
+    <li key={album.id}>{album.title}</li>
+  ))
+
+  return <div>{albumLists}</div>
 }
---
+++ src/services/AlbumService.ts
@@ -0,0 +1,41 @@
+import { Observable } from 'rxjs/internal/Observable'
+import { Subscriber } from 'rxjs/internal/Subscriber'
+
+export default class AlbumService {
+    constructor() {
+        const token = localStorage.getItem('token')
+        console.log(token)
+    }
+
+    getAllAlbums(): Observable<Album[]> {
+        const url: string = `https://jsonplaceholder.typicode.com/albums`
+
+        return new Observable((observer: Subscriber<Album[]>) => {
+            fetch(url)
+                .then((response) => response.json())
+                .then((json) => {
+                    observer.next(json)
+                    observer.complete()
+                })
+        })
+    }
+
+    getAlbumById(id: number): Observable<Album> {
+        const url: string = `https://jsonplaceholder.typicode.com/albums/${id}`
+
+        return new Observable((observer: Subscriber<Album>) => {
+            fetch(url)
+                .then((response) => response.json())
+                .then((json) => {
+                    observer.next(json)
+                    observer.complete()
+                })
+        })
+    }
+}
+
+export interface Album {
+    id: number
+    title: string
+    userId: number
+}
