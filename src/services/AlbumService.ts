9ceb9726ff672bd0571a541b119d51c5f09de77f
Dika Handika <dika.handika@paper.id>
2021-05-26 14:43:44 +0700

    formatting existing services

--- src/services/AlbumService.ts
+++ src/services/AlbumService.ts
@@ -2,39 +2,39 @@ import { Observable } from 'rxjs/internal/Observable'
 import { Subscriber } from 'rxjs/internal/Subscriber'
 
 export default class AlbumService {
-    // constructor() {
-    //     const token = localStorage.getItem('token')
-    // }
+  // constructor() {
+  //     const token = localStorage.getItem('token')
+  // }
 
-    getAllAlbums(): Observable<Album[]> {
-        const url: string = `https://jsonplaceholder.typicode.com/albums`
+  getAllAlbums(): Observable<Album[]> {
+    const url: string = `https://jsonplaceholder.typicode.com/albums`
 
-        return new Observable((observer: Subscriber<Album[]>) => {
-            fetch(url)
-                .then((response) => response.json())
-                .then((json) => {
-                    observer.next(json)
-                    observer.complete()
-                })
+    return new Observable((observer: Subscriber<Album[]>) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          observer.next(json)
+          observer.complete()
         })
-    }
+    })
+  }
 
-    getAlbumById(id: number): Observable<Album> {
-        const url: string = `https://jsonplaceholder.typicode.com/albums/${id}`
+  getAlbumById(id: number): Observable<Album> {
+    const url: string = `https://jsonplaceholder.typicode.com/albums/${id}`
 
-        return new Observable((observer: Subscriber<Album>) => {
-            fetch(url)
-                .then((response) => response.json())
-                .then((json) => {
-                    observer.next(json)
-                    observer.complete()
-                })
+    return new Observable((observer: Subscriber<Album>) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          observer.next(json)
+          observer.complete()
         })
-    }
+    })
+  }
 }
 
 export interface Album {
-    id: number
-    title: string
-    userId: number
+  id: number
+  title: string
+  userId: number
 }
--- src/services/TodoService.ts
+++ src/services/TodoService.ts
@@ -2,40 +2,40 @@ import { Observable } from 'rxjs/internal/Observable'
 import { Subscriber } from 'rxjs/internal/Subscriber'
 
 export default class TodoService {
-    // constructor() {
-    //     const token = localStorage.getItem('token')
-    // }
+  // constructor() {
+  //     const token = localStorage.getItem('token')
+  // }
 
-    getAllTodos(): Observable<Todo[]> {
-        const url: string = `https://jsonplaceholder.typicode.com/todos`
+  getAllTodos(): Observable<Todo[]> {
+    const url: string = `https://jsonplaceholder.typicode.com/todos`
 
-        return new Observable((observer: Subscriber<Todo[]>) => {
-            fetch(url)
-                .then((response) => response.json())
-                .then((json) => {
-                    observer.next(json)
-                    observer.complete()
-                })
+    return new Observable((observer: Subscriber<Todo[]>) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          observer.next(json)
+          observer.complete()
         })
-    }
+    })
+  }
 
-    getTodoById(id: number): Observable<Todo> {
-        const url: string = `https://jsonplaceholder.typicode.com/todos/${id}`
+  getTodoById(id: number): Observable<Todo> {
+    const url: string = `https://jsonplaceholder.typicode.com/todos/${id}`
 
-        return new Observable((observer: Subscriber<Todo>) => {
-            fetch(url)
-                .then((response) => response.json())
-                .then((json) => {
-                    observer.next(json)
-                    observer.complete()
-                })
+    return new Observable((observer: Subscriber<Todo>) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          observer.next(json)
+          observer.complete()
         })
-    }
+    })
+  }
 }
 
 export interface Todo {
-    completed: boolean
-    id: number
-    title: string
-    userId: number
+  completed: boolean
+  id: number
+  title: string
+  userId: number
 }
