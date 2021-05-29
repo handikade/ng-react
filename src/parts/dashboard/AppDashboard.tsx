f17d81febd915689b294e507e8bc2d43ff7a3ad0
Dika Handika <dika.handika@paper.id>
2021-05-29 18:12:17 +0700

    appbar

--- src/parts/dashboard/AppDashboard.tsx
+++ src/parts/dashboard/AppDashboard.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
-import { Container } from '@material-ui/core'
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  makeStyles,
+  Toolbar,
+  Typography
+} from '@material-ui/core'
 import AppBottomNavigation from './../../parts/bottom-navigation/AppBottomNavigation'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}))
 
 export default function AppDashboard(props: {
   children:
@@ -14,10 +35,30 @@ export default function AppDashboard(props: {
     | null
     | undefined
 }) {
+  const classes = useStyles()
+
   return (
-    <Container maxWidth="sm">
-      {props.children}
-      <AppBottomNavigation></AppBottomNavigation>
-    </Container>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm">
+        {props.children}
+        <AppBottomNavigation></AppBottomNavigation>
+      </Container>
+    </>
   )
 }
